99ef47e6e7a056786e586e1f87f7187dd4d8e339
Asifmikdad <asifmikdad@gmail.com>
2022-05-08 11:21:52 +0600

    simple if else problem

---
+++ if_else.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int marks;
+    printf("Enter your marks: ");
+    scanf("%d", &marks);
+
+    if (marks >= 40){
+        printf("You have passed");
+
+    }
+    else{ 
+        printf("You have failed");
+    }
+    return 0;
+}
