d71d34f8e31a3ff49138ec712e46baebf37b0a0c
Asifmikdad <asifmikdad@gmail.com>
2022-05-08 19:28:48 +0600

    nested if else

--- nested_if_else.c
+++ nested_if_else.c
@@ -1,18 +1,22 @@
-#include<stdio.h>
+#include <stdio.h>
 int main(int argc, char const *argv[])
 {
     int a, b;
     printf("Enter two numbers: ");
     scanf("%d %d", &a, &b);
-    if(b != 0){
-        if(a % b == 0){
+    if (b != 0)
+    {
+        if (a % b == 0)
+        {
             printf("%d is divisible by %d", a, b);
         }
-        else{
+        else
+        {
             printf("%d is not divisible by %d", a, b);
         }
     }
-    else{
+    else
+    {
         printf("You can not divide by 0");
     }
 
